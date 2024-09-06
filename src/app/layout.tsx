ba827f50ca6af1ad3e043381021a1b1b31d45434
HzDev3628 <artem.khar5uk@gmail.com>
2024-09-06 15:22:44 +0300

    minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
-import { Header } from '@/app/widgets'
+import { Header } from '@/app/_widgets'
 
 const nippo = localFont({
   src: [
